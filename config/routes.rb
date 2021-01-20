c7e34f0cb9dea159478ab2f44d62dc35b4afb97f
Ahmed Amin <qa.ahmed.amin10@gmail.com>
2021-01-20 23:11:05 +0300

    finish the project

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+
   root "articles#home"
 
-  get "/articles", to: "articles#index"
+  
+  resources :articles do
+    resources :comments
+  end
+
+  # get "/articles", to: "articles#index"
+  # get "/articles/:id", to: "articles#show"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
